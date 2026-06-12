738d80865c69264ad26df36072e44c969eb40a1b
Merin George P <merin@Merins-MacBook-Air.local>
2026-06-13 02:11:48 +0530

    Initial commit

---
+++ scenes/main_character.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+const SPEED = 400.0
+const JUMP_VELOCITY = -900.0
+
+@onready var sprite_2d = $Sprite2D
+
+func _physics_process(delta: float) -> void:
+	
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "running"
+	else:
+		sprite_2d.animation = "default"
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		sprite_2d.animation = "jumping"
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Movement
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Flip sprite
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
+
+	move_and_slide()
